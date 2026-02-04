6a76077b8d6ac866061e3df822f070ac7385e858
Murilo <mbalves1@outlook.com>
2026-02-04 18:07:52 -0300

    refactor: update main.ts for improved code structure and readability

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
-import express, { Response, Request } from 'express'
-import crypto from 'crypto'
-const app = express()
+import express, { Response, Request } from 'express';
+import crypto from 'crypto';
+import pgp from 'pg-promise';
+const app = express();
+app.use(express.json());
+
+const connection = pgp()('postgres://postgres:123456@localhost:5432/app');
 
 app.post('/signup', async (req: Request, res: Response) => {
-  console.log('/signup')
-  const accountId = crypto.randomUUID()
-  res.json({
-    accountId,
-  })
-})
+	console.log('/signup');
+	const accountId = crypto.randomUUID();
+	res.json({
+		accountId,
+	});
+});
 
 app.get('/accounts/:accountId', async (req: Request, res: Response) => {
-  const accountId = req.params.accountId
-  console.log(`/accounts/${accountId}`)
+	const accountId = req.params.accountId;
+	console.log(`/accounts/${accountId}`);
+	const [account] = await connection.query('select * from cccar.account', []);
+	console.log(account);
+	res.json({
+		accountId,
+	});
+});
 
-  res.json({
-    accountId,
-  })
-})
-
-app.listen(3000)
+app.listen(3000);
