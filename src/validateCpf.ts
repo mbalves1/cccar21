1e16d0b63bc8d27ecda262725fffc5567ad1a357
Murilo <mbalves1@outlook.com>
2026-02-07 11:00:29 -0300

    refactor: simplify CPF validation logic and improve readability

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,57 +1,28 @@
-export function validateCpf(str: string) {
-	if (str !== null) {
-		if (str !== undefined) {
-			if (str.length >= 11 && str.length <= 14) {
-				// cleaning cpf
-				str = str
-					.replace('.', '')
-					.replace('.', '')
-					.replace('-', '')
-					.replace(' ', '');
-				// tudo igual
-				if (!str.split('').every((c) => c === str[0])) {
-					try {
-						let d1, d2;
-						let dg1, dg2, rest;
-						let digito;
-						let nDigResult;
-						d1 = d2 = 0;
-						dg1 = dg2 = rest = 0;
+export function validateCpf(cpf: string) {
+	if (!cpf) return false;
+	cpf = cpf.replace(/\D/g, '');
+	if (cpf.length !== 11) return false;
+	if (cpf.split('').every((c) => c === cpf[0])) return false;
+	let d1 = 0;
+	let d2 = 0;
+	let dg1 = 0;
+	let dg2 = 0;
+	let rest = 0;
+	let digito;
 
-						for (let nCount = 1; nCount < str.length - 1; nCount++) {
-							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-							// 	return false;
-							// } else {
+	for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
+		digito = parseInt(cpf.substring(nCount - 1, nCount));
+		d1 = d1 + (11 - nCount) * digito;
+		d2 = d2 + (12 - nCount) * digito;
+	}
 
-							digito = parseInt(str.substring(nCount - 1, nCount));
-							d1 = d1 + (11 - nCount) * digito;
+	rest = d1 % 11;
+	dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
+	d2 += 2 * dg1;
+	rest = d2 % 11;
+	if (rest < 2) dg2 = 0;
+	else dg2 = 11 - rest;
 
-							d2 = d2 + (12 - nCount) * digito;
-							// }
-						}
-
-						rest = d1 % 11;
-
-						// se for menor que 2 é 0, senão é 11 menos o resto
-						dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
-						d2 += 2 * dg1;
-						rest = d2 % 11;
-						if (rest < 2) dg2 = 0;
-						else dg2 = 11 - rest;
-
-						let nDigVerific = str.substring(str.length - 2, str.length);
-						nDigResult = '' + dg1 + '' + dg2;
-
-						return nDigVerific == nDigResult;
-
-						// se der problema...
-					} catch (e) {
-						console.error('Erro !' + e);
-
-						return false;
-					}
-				} else return false;
-			} else return false;
-		} else return false;
-	} else return false;
+	let nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
+	return nDigVerific == `${dg1}${dg2}`;
 }
