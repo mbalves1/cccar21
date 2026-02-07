27428c6770a957dee083d71fd5f07cee4c9430d0
Murilo <mbalves1@outlook.com>
2026-02-07 11:26:03 -0300

    test: update CPF validation tests for consistency and clarity

--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -5,13 +5,13 @@ test.each(['078.300.210-66', '562.523.340-30', '639.951.250-69'])(
 	(cpf: string) => {
 		const isValid = validateCpf(cpf);
 		expect(isValid).toBe(true);
-	}
+	},
 );
 
-test.each(['11111111111', '560-30', '639.001.000-00'])(
+test.each([null, undefined, '11111111111', '560-30', '639.001.000-00'])(
 	'Deve testar um cpf invalido: %s',
-	(cpf: string) => {
+	(cpf: any) => {
 		const isValid = validateCpf(cpf);
 		expect(isValid).toBe(false);
-	}
+	},
 );
