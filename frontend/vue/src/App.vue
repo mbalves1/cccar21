04a010450b8edf1e7ca22ca221cf846360235f2c
Murilo <mbalves1@outlook.com>
2026-02-11 08:25:26 -0300

    feat: update App component and corresponding test to use h1 tag

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>Ola Murilo</div>
+	<h1>Ola Murilo</h1>
 </template>
 
 <style scoped></style>
--- frontend/vue/src/__tests__/App.spec.ts
+++ frontend/vue/src/__tests__/App.spec.ts
@@ -1,7 +1,11 @@
 import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import App from '../App.vue';
 
 describe('Deve testar o componente principal', () => {
 	it('deve validar 1 igual a 1', () => {
-		expect(1).toBe(1);
+		const wrapper = mount(App, {});
+		console.log('wrapper', wrapper.html);
+		expect(wrapper.get('h1').text()).toBe('Ola Murilo');
 	});
 });
