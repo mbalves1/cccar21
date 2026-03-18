207b6a609f78a5b27bc7e31c7f0ce9ff38bddd53
Murilo <mbalves1@outlook.com>
2026-03-18 08:23:16 -0300

    [feat]: criando fluxo de place order, repository e order

---
+++ backend/src/application/usecase/PlaceOrder.ts
@@ -0,0 +1,38 @@
+import AccountRepository from '../../infra/repository/AccountRepository';
+import { inject } from '../../infra/di/Registry';
+import OrderRepository from '../../infra/repository/OrderRepository';
+import Order from '../../domain/Order';
+
+export default class PlaceOrder {
+	@inject('accountRepository')
+	accountRepository!: AccountRepository;
+	@inject('orderRepository')
+	orderRepository!: OrderRepository;
+
+	async execute(input: Input): Promise<Output> {
+		const account = await this.accountRepository.getById(input.accountId);
+		const order = Order.create(
+			input.accountId,
+			input.marketId,
+			input.side,
+			input.quantity,
+			input.price,
+		);
+		this.orderRepository.save(order);
+		return {
+			orderId: order.orderId,
+		};
+	}
+}
+
+type Input = {
+	accountId: string;
+	marketId: string;
+	side: string;
+	quantity: number;
+	price: number;
+};
+
+type Output = {
+	orderId: string;
+};
---
+++ backend/src/infra/repository/OrderRepository.ts
@@ -0,0 +1,42 @@
+import Order from '../../domain/Order';
+import DatabaseConnection from '../database/DatabaseConnection';
+import { inject } from '../di/Registry';
+
+export default interface OrderRepository {
+	save(order: Order): Promise<void>;
+	getById(orderId: string): Promise<Order>;
+}
+
+export class OrderRepositoryDatabase implements OrderRepository {
+	@inject('databaseConnection')
+	connection!: DatabaseConnection;
+
+	async save(order: Order): Promise<void> {
+		await this.connection.query(
+			'insert into cccar.order (order_id, account_id, market_id, side, quantity, price, fill_quantity, fill_price, status, timestamp) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+			[],
+		);
+	}
+
+	async getById(orderId: string): Promise<Order> {
+		const [orderData] = await this.connection.query(
+			'select * from cccar.order where order_id = $1',
+			[orderId],
+		);
+		if (!orderData) {
+			throw new Error('Order not found');
+		}
+		return new Order(
+			orderData.order_id,
+			orderData.account_id,
+			orderData.market_id,
+			orderData.side,
+			parseFloat(orderData.quantity),
+			parseFloat(orderData.price),
+			parseFloat(orderData.fill_quantity),
+			parseFloat(orderData.fill_price),
+			orderData.status,
+			orderData.timestamp,
+		);
+	}
+}
