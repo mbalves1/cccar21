125fdc418c192ee4634c0e7f8564800a3dd0e759
Murilo <mbalves1@outlook.com>
2026-04-20 08:16:03 -0300

    feat: add integration test for account persistence in ORM

---
+++ backend/test/integration/ORM.test.ts
@@ -0,0 +1,26 @@
+import { PgPromiseAdapter } from '../../src/infra/database/DatabaseConnection';
+import Registry from '../../src/infra/di/Registry';
+
+test('Deve persistir uma conta', async () => {
+	const connection = new PgPromiseAdapter();
+	Registry.getInstance().provide('databaseConnection', connection);
+	const orm = new ORM();
+	const accountId = crypto.randomUUID();
+	const accountModel = new AccountModel(
+		accountId,
+		'John Doe',
+		'john.doe@gmail.com',
+		'11111111111',
+		'asqQwe123',
+	);
+	await orm.save(accountModel);
+	const savedAccountModel = await orm.get(
+		accountModel,
+		'account_id',
+		accountId,
+	);
+	expect(savedAccountModel.name).toBe(accountModel.name);
+	expect(savedAccountModel.email).toBe(accountModel.email);
+	expect(savedAccountModel.document).toBe(accountModel.document);
+	expect(savedAccountModel.password).toBe(accountModel.password);
+});
